f369a0b8085aa2e2f989ea6ae84ac950cbf03174
nkmwambs <nkmwambs@gmail.com>
2019-08-21 10:12:55 +0300

    Added tabs state save at global level

--- application/views/backend/modal.php
+++ application/views/backend/modal.php
@@ -224,6 +224,21 @@ jQuery(document).ready(function($)
       ],
       'stateSave':true
   });
+
+
+  if (location.hash) {
+        $("a[href='" + location.hash + "']").tab("show");
+    }
+    $(document.body).on("click", "a[data-toggle]", function(event) {
+        location.hash = this.getAttribute("href");
+    });
+
+  $(window).on("popstate", function() {
+      var anchor = location.hash || $("a[data-toggle='tab']").first().attr("href");
+      $("a[href='" + anchor + "']").tab("show");
+
+  });
+
 });
 
 </script>
