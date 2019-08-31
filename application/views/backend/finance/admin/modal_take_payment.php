1f9157470a919093ed89661ffa61dfd52e52ff2e
nkmwambs <nkmwambs@gmail.com>
2019-08-31 22:17:32 +0300

    Automatically show the student as the payee when taking payment by default

--- application/views/backend/finance/admin/modal_take_payment.php
+++ application/views/backend/finance/admin/modal_take_payment.php
@@ -1,7 +1,12 @@
-<?php 
+<?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+$this->db->select(array('student.name as student','invoice.student_id as student_id','invoice_id',
+'invoice.class_id as class_id','yr','term','amount','amount_due','creation_timestamp','status',
+'carry_forward','transitioned','last_approval_request_id'));
+$this->db->join('student','student.student_id=invoice.student_id');
 $edit_data	=	$this->db->get_where('invoice' , array('invoice_id' => $param2) )->result_array();
+
 $row = $edit_data[0];
 
 //foreach ($edit_data as $row):
@@ -17,13 +22,13 @@ $row = $edit_data[0];
 				<?php echo form_open(base_url() . 'index.php?finance/record_fees_income/take_payment/'.$row['invoice_id'], array(
 					'class' => 'form-horizontal form-groups-bordered validate','target'=>'_top','id'=>'frm_payment'));?>
 
-		
+
 		           <input type="hidden" class="form-control" value="<?php echo $row['amount_due'];?>" readonly/>
 		           <input type="hidden" class="form-control" name="amount_paid" value="<?php echo $this->crud_model->fees_paid_by_invoice($param2);?>" readonly/>
 					<?php $bal = $this->crud_model->fees_balance_by_invoice($param2);?>
 		           <input type="hidden" id="get_bal" class="form-control" value="<?php echo $bal;?>" readonly/>
 
-		            
+
 		            <div class="form-group">
 		                <label class="col-sm-offset-6 control-label"><?php echo get_phrase('payment');?></label>
 		            </div>
@@ -44,11 +49,11 @@ $row = $edit_data[0];
 									<?php
 										$this->db->select(array('fees_structure_details.detail_id','fees_structure_details.name',
 	                                	'fees_structure_details.amount','invoice_details.invoice_details_id','invoice_details.amount_due'));
-	                                	
-										$this->db->join('fees_structure_details','fees_structure_details.detail_id=invoice_details.detail_id');									
+
+										$this->db->join('fees_structure_details','fees_structure_details.detail_id=invoice_details.detail_id');
 	                                	$this->db->join('fees_structure','fees_structure.fees_id=fees_structure_details.fees_id');
 	                                	$invoice_details = $this->db->get_where("invoice_details",array('invoice_details.invoice_id'=>$row['invoice_id']))->result_object();
-																				
+
 										foreach($invoice_details as $inv):
 									?>
 										<tr>
@@ -58,9 +63,9 @@ $row = $edit_data[0];
 											<td><?php echo number_format($this->crud_model->fees_balance_by_invoice_detail($inv->invoice_details_id),2);?></td>
 											<td><input type="text" onkeyup="return get_total_payment();" class="form-control paying" name="take_payment[<?php echo $inv->invoice_details_id;?>]" id="" value="0"/></td>
 										</tr>
-									
+
 									<?php
-										
+
 										endforeach;
 										$tot_due = array_sum(array_column($invoice_details, 'amount_due'));
 										$tot_paid = $this->crud_model->fees_paid_by_invoice($param2);//array_sum(array_column($invoice_details, 'amount_paid'));
@@ -84,11 +89,11 @@ $row = $edit_data[0];
 		                    <input type="text" required="required" class="form-control" name="description" id="description" placeholder="<?php echo get_phrase('description');?>"/>
 		                </div>
 		            </div>
-		            
+
 		            <div class="form-group">
 		                <label class="col-sm-3 control-label"><?php echo get_phrase('payee');?></label>
 		                <div class="col-sm-6">
-		                    <input type="text" required="required" class="form-control" name="payee" id="payee" placeholder="<?php echo get_phrase('payee');?>"/>
+		                    <input type="text" value="<?=$row['student']?>" required="required" class="form-control" name="payee" id="payee" placeholder="<?php echo get_phrase('payee');?>"/>
 		                </div>
 		            </div>
 
@@ -97,13 +102,13 @@ $row = $edit_data[0];
                         <div class="col-sm-6">
                             <select name="method" id="method" class="form-control" required="required">
                                 <option value="1"><?php echo get_phrase('cash');?></option>
-                                <option value="2"><?php echo get_phrase('cheque');?></option>
+                                <option value="2"><?php echo get_phrase('banked');?></option>
 								<option value="2"><?php echo get_phrase('lipa_karo_mpesa');?></option>
 								<!-- <option value="2"><?php echo get_phrase('mpesa_paybill');?></option> -->
                             </select>
                         </div>
                     </div>
-                    
+
                     <div class="form-group" style="display: none;">
 		                <label class="col-sm-3 control-label"><?php echo get_phrase('reference_number');?></label>
 		                <div class="col-sm-6">
@@ -114,20 +119,20 @@ $row = $edit_data[0];
                     <div class="form-group">
 	                    <label class="col-sm-3 control-label"><?php echo get_phrase('date');?></label>
 	                    <div class="col-sm-6">
-	                        <input type="text" class="datepicker form-control" readonly="readonly" 
-	                        	data-start-date="<?php echo $this->crud_model->next_transaction_date()->start_date;?>" 
-	                        	data-end-date="<?php echo $this->crud_model->next_transaction_date()->end_date;?>" 
-	                        	data-format="yyyy-mm-dd" name="timestamp" 
+	                        <input type="text" class="datepicker form-control" readonly="readonly"
+	                        	data-start-date="<?php echo $this->crud_model->next_transaction_date()->start_date;?>"
+	                        	data-end-date="<?php echo $this->crud_model->next_transaction_date()->end_date;?>"
+	                        	data-format="yyyy-mm-dd" name="timestamp"
 	                            value="" required="required"/>
 	                    </div>
 					</div>
-					
+
 					<div class="form-group">
 						<label for="field-2" class="col-sm-3 control-label"><?php echo get_phrase('serial_number');?></label>
-                        
+
 						<div class="col-sm-6">
 							<input type="text" class="form-control" readonly="readonly" value="<?=$this->crud_model->next_batch_number();?>" required="required">
-						</div> 
+						</div>
 				</div>
 
                     <input type="hidden" name="invoice_id" value="<?php echo $row['invoice_id'];?>">
@@ -161,41 +166,41 @@ $row = $edit_data[0];
 	function get_total_payment(){
 		var tot = 0;
 		$('.paying').each(function(){
-			
+
 			var amt = 0;
-			
+
 			if(!isNaN(parseInt($(this).val()))/2){
 				amt = $(this).val();
 			}
-			
+
 			tot = parseInt(tot)+parseInt(amt);
 		});
-		
+
 		//alert(tot);
-		
-		$("#total_payment").val(tot);		
-		
+
+		$("#total_payment").val(tot);
+
 		if(parseInt(tot) == parseInt(<?=$this->crud_model->fees_balance_by_invoice($param2);?>)){
-			$(".overpay").removeAttr('readonly');	
+			$(".overpay").removeAttr('readonly');
 		}else if(parseInt(tot) < parseInt(<?=$this->crud_model->fees_balance_by_invoice($param2);?>)){
 			$(".overpay").val(0);
 			$(".overpay").prop("readonly","readonly");
 		}
 	}
-	
-	
+
+
 	$(".paying").change(function(){
 		var detail_balance = $(this).parent().prev().html();
 		var detail_paying = $(this).val();
-		
+
 		if((parseInt(detail_paying) - parseInt($("#overpayment").val())) > parseInt(accounting.unformat(detail_balance))){
 			alert("<?=get_phrase("paying_more_than_balance");?>");
 			$(this).val("0");
 			get_total_payment();
 			return false;
 		}
-		
+
 	});
 
-	
+
 </script>
