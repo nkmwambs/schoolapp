a97370aa448009707937cdd40eb9034e3a0c153c
nkmwambs <nkmwambs@gmail.com>
2019-09-04 22:18:53 +0300

    Removed button color on the unpaid invoices

--- application/views/backend/finance/admin/invoices_unpaid.php
+++ application/views/backend/finance/admin/invoices_unpaid.php
@@ -13,11 +13,11 @@ if (!defined('BASEPATH')) {
 
 <div class="row">
 	<div class="col-xs-12 text-center" style="align:center;padding:5px;margin-bottom: 25px;">
-		<a href="<?= base_url(); ?>index.php?finance/student_collection_tally/<?= date('Y'); ?>" class="btn btn-info btn-icon"> <i class="fa fa-list"></i> <?= get_phrase('payment_tally_sheet'); ?></a>
+		<a href="<?= base_url(); ?>index.php?finance/student_collection_tally/<?= date('Y'); ?>" class="btn btn-default"> <i class="fa fa-list"></i> <?= get_phrase('payment_tally_sheet'); ?></a>
 		<?php $count_to_notify = $this -> db -> get_where('invoice', array('status' => 'unpaid')) -> num_rows(); ?>
-		<a href="#" onclick="confirm_action('<?= base_url(); ?>index.php?finance/sms_fee_balances');" class="btn btn-info btn-icon"><i class="fa fa-mobile"></i><?= get_phrase('SMS_balances'); ?><span class="badge badge-primary"><?= $count_to_notify; ?></span></a>
-		<a href="<?php echo base_url(); ?>index.php?finance/create_invoice" class="btn btn-info btn-icon"><i class="fa fa-money"></i><?= get_phrase('create_invoice'); ?></a>
-		<a href="<?php echo base_url(); ?>index.php?finance/missing_invoices" class="btn btn-info  btn-icon"><i class="fa fa-times"></i><?= get_phrase('missing_invoices'); ?></a>
+		<a href="#" onclick="confirm_action('<?= base_url(); ?>index.php?finance/sms_fee_balances');" class="btn btn-default"> <i class="fa fa-mobile"></i> <?= get_phrase('SMS_balances'); ?> <span class="badge badge-warning"><?= $count_to_notify; ?></span></a>
+		<a href="<?php echo base_url(); ?>index.php?finance/create_invoice" class="btn btn-default"> <i class="fa fa-money"></i> <?= get_phrase('create_invoice'); ?></a>
+		<a href="<?php echo base_url(); ?>index.php?finance/missing_invoices" class="btn btn-default"> <i class="fa fa-times"></i> <?= get_phrase('missing_invoices'); ?></a>
 	</div>
 </div>
 
