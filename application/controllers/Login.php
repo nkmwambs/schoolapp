a9fdc4e538ead12039f7df51e3efd7fae8fbaf18
nkmwambs <nkmwambs@gmail.com>
2019-09-20 15:53:47 +0300

    Uncommented database load in login controller

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -16,9 +16,9 @@ class Login extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->load->model('crud_model');
-        //$this->load->database();
+        $this->load->database();
         $this->load->library('session');
-		$this->config->load('localrepositoryvars');
+		    $this->config->load('localrepositoryvars');
         /* cache control */
         $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
         $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
