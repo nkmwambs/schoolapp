2d6d5c48ea93b815e1b5c26723cd39ca3b76e62e
nkmwambs <nkmwambs@gmail.com>
2019-10-18 16:59:19 +0300

    Testing if git syncs

--- application/views/backend/login.php
+++ application/views/backend/login.php
@@ -7,13 +7,13 @@
 	?>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	
+
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="description" content="Neon Admin Panel" />
 	<meta name="author" content="" />
-	
+
 	<title><?php echo get_phrase('login');?> | <?php echo $system_title;?></title>
-	
+
 
 	<link rel="stylesheet" href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
 	<link rel="stylesheet" href="assets/css/font-icons/entypo/css/entypo.css">
@@ -34,7 +34,7 @@
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
 	<link rel="shortcut icon" href="assets/images/favicon.png">
-	
+
 </head>
 <body class="page-body login-page login-form-fall" data-url="http://neon.dev">
 
@@ -45,90 +45,90 @@ var baseurl = '<?php echo base_url();?>';
 </script>
 
 <div class="login-container">
-	
+
 	<div class="login-header login-caret">
-		
+
 		<div class="login-content" style="width:100%;">
-			
+
 			<a href="<?php echo base_url();?>" class="logo">
 				<img src="uploads/logo.png" height="140" alt="" />
 			</a>
-			
+
 			<p class="description">
             	<h2 style="color:#cacaca; font-weight:100;">
 					<?php echo $system_name;?>
               </h2>
            </p>
-			
+
 			<!-- progress bar indicator -->
 			<div class="login-progressbar-indicator">
 				<h3>43%</h3>
 				<span>logging in...</span>
 			</div>
 		</div>
-		
+
 	</div>
-	
+
 	<div class="login-progressbar">
 		<div></div>
 	</div>
-	
+
 	<div class="login-form">
-		
+
 		<div class="login-content">
-			
+
 			<div class="form-login-error">
 				<h3>Invalid login</h3>
 				<p>Please enter correct email and password!</p>
 			</div>
-			
+
 			<form method="post" role="form" id="form_login">
-				
+
 				<div class="form-group">
-					
+
 					<div class="input-group">
 						<div class="input-group-addon">
 							<i class="entypo-user"></i>
 						</div>
-						
-						<input type="text" class="form-control" name="email" id="email" placeholder="Email" autocomplete="off" data-mask="email" />
+
+						<input type="text" class="form-control" name="email" id="email" placeholder="Personal Email" autocomplete="off" data-mask="email" />
 					</div>
-					
+
 				</div>
-				
+
 				<div class="form-group">
-					
+
 					<div class="input-group">
 						<div class="input-group-addon">
 							<i class="entypo-key"></i>
 						</div>
-						
+
 						<input type="password" class="form-control" name="password" id="password" placeholder="Password" autocomplete="off" />
 					</div>
-				
+
 				</div>
-				
+
 				<div class="form-group">
 					<button type="submit" class="btn btn-primary btn-block btn-login">
 						<i class="entypo-login"></i>
 						Login
 					</button>
 				</div>
-				
-						
+
+
 			</form>
-			
-			
+
+
 			<div class="login-bottom-links">
 				<a href="<?php echo base_url();?>index.php?login/forgot_password" class="link">
 					<?php echo get_phrase('forgot_your_password');?> ?
 				</a>
 			</div>
-			
+
 		</div>
-		
+
 	</div>
-	
+
 </div>
 
 
