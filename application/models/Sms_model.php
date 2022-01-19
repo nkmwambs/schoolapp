0902b507624b8eb70a2af07c63855ffa31a60ed8
Nicodemus Karisa <NKarisa@us.ci.org>
2022-01-19 16:39:04 +0300

    Update sms code - 7

--- application/models/Sms_model.php
+++ application/models/Sms_model.php
@@ -19,7 +19,8 @@ class Sms_model extends CI_Model {
         if ($active_sms_service == '' || $active_sms_service == 'disabled')
             return;
 		if ($active_sms_service == 'africastalking') {
-            $this->send_sms_via_africastalking($message , $reciever_phone );
+            //$this->send_sms_via_africastalking($message , $reciever_phone );
+            return "Find me";
         }
         if ($active_sms_service == 'clickatell') {
             $this->send_sms_via_clickatell($message , $reciever_phone );
