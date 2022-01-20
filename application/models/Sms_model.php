b78f14d2e3dff396c6334cca7d3df80991b8cc88
Nicodemus Karisa <NKarisa@us.ci.org>
2022-01-20 12:14:16 +0300

    Updated sender id

--- application/models/Sms_model.php
+++ application/models/Sms_model.php
@@ -34,7 +34,7 @@ class Sms_model extends CI_Model {
 
         $account_apikey   = $this->db->get_where('settings', array('type' => 'africastalking_api_id'))->row()->description;
         $username     = $this->db->get_where('settings', array('type' => 'africastalking_user'))->row()->description;
-        $from     = '';//$this->db->get_where('settings', array('type' => 'africastalking_sender_id'))->row()->description;
+        $from     = $this->db->get_where('settings', array('type' => 'africastalking_sender_id'))->row()->description;
 		        
                     // $this->config->set_item('username', $username);
                     // $this->config->set_item('apiKey', $account_apikey);
